ad42cb8e2b009206af9e36e2a6993052b6b40b7c
Randoragon <randoragongamedev@gmail.com>
2021-03-26 15:46:25 +0100

    Add doxygen.h pages documentation
    I've only written a brief introduction to the actual documentation that
    I'll write, but more detail will have to wait until I actually consider
    the program complete.

---
+++ doxygen.h
@@ -0,0 +1,37 @@
+/** @mainpage Documentation Main Page
+ *
+ * @section intro Introduction
+ *
+ * You are reading the documentation of the @c iniget command-line utility program.
+ * It is targeted at developers trying to understand the inner-workings
+ * of the program, not casual users (for that you can see
+ * <a href="https://github.com/randoragon/iniget/tree/main/README.MD">README</a>).
+ * 
+ * @c iniget is not a complicated program, but its code base is messy enough to
+ * make reading a bit difficult, and orderly enough to benefit from a good
+ * explanation of its components.
+ *
+ * @section toc Table of Contents
+ *
+ * Note that this Table of Contents lists only documentation written as separate
+ * pages of information, to form a foundation for understanding the actual source code.
+ * These pages, however, are only a small part of the entire documentation.
+ * A substantial chunk is written for specific structures, functions and variables only,
+ * so be sure to refer to those in order to fill some gaps.
+ *
+ * 1. @ref main
+ *
+ * @section Credits
+ *
+ * (C) Copyright 2021 Randoragon. MIT License.
+ *
+ * - Email: randoragongamedev@gmail.com
+ * - Discord: Randoragon#1958
+ * - Discord Server: https://discord.gg/YqJYzMS
+ * - GitHub: https://github.com/randoragon
+ */
+
+/** @page main Program Anatomy and Design
+ *
+ * work in progress
+ */
